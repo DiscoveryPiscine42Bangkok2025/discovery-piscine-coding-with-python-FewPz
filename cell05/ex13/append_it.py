24349b29c73d093024880aafcb31cab3f9117f1a
FewPz <sharpkung4826@gmail.com>
2025-09-14 16:50:07 +0700

    done cell05-ex13

---
+++ cell05/ex13/append_it.py
@@ -0,0 +1,13 @@
+import sys
+
+args = sys.argv[1:]
+WORD = "ism"
+
+if len(args) < 1:
+    print("none")
+else:
+    for arg in args:
+        if WORD in arg:
+            continue
+        arg += WORD
+        print(arg)
