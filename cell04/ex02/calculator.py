459867ba59ebb61e32179f1d1dffcab820438835
FewPz <sharpkung4826@gmail.com>
2025-09-14 14:04:25 +0700

    done cell04-ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,7 @@
+first_number = int(input("Give me the first number: "))
+second_number = int(input("Give me the second number: "))
+print("Thank you!")
+print("%d + %d = %d" % (first_number, second_number, first_number + second_number))
+print("%d - %d = %d" % (first_number, second_number, first_number - second_number))
+print("%d / %d = %d" % (first_number, second_number, first_number / second_number))
+print("%d * %d = %d" % (first_number, second_number, first_number * second_number))
