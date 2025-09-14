d2532641bddf1888631767926140ad112fedc9ef
FewPz <sharpkung4826@gmail.com>
2025-09-14 16:50:57 +0700

    done cell07-ex03

---
+++ cell07/ex03/persons_of_interest.py
@@ -0,0 +1,15 @@
+def famous_births(persons):
+    sorted_persons = sorted(persons.values(), key=lambda p: int(p["date_of_birth"]))
+    for person in sorted_persons:
+        name = person["name"]
+        birth_year = person["date_of_birth"]
+        print(f"{name} is a great scientist born in {birth_year}.")
+
+women_scientists = {
+    "ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
+    "cecilia": { "name": "Cecila Payne", "date_of_birth": "1900" },
+    "lise": { "name": "Lise Meitner", "date_of_birth": "1878" },
+    "grace": { "name": "Grace Hopper", "date_of_birth": "1906" }
+}
+
+famous_births(women_scientists)
