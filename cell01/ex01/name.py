632bae11f7635efeb902ba508a73ec323d4513d6
FewPz <sharpkung4826@gmail.com>
2025-09-15 21:11:45 +0700

    fix variable naming for consistency in name.py

--- cell01/ex01/name.py
+++ cell01/ex01/name.py
@@ -1,4 +1,4 @@
-firstname = "Peeranat"
-lastname = "Matsor"
-whole_name = firstname + " " + lastname
+first_name = "Peeranat"
+last_name = "Matsor"
+whole_name = first_name + " " + last_name
 print(whole_name)
